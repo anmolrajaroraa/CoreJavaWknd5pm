e6149296c4f93b741836a9b0031631f8a8e63548
Anmol Arora <anmolarora1711@gmail.com>
2020-09-13 11:11:28 +0530

    Code

--- BillingSystem/src/com/company/billing/main/BillingScreen.java
+++ BillingSystem/src/com/company/billing/main/BillingScreen.java
@@ -2,13 +2,28 @@ package com.company.billing.main;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.company.billing.model.Product;
+import com.company.billing.model.ProductDAO;
+
 import javax.swing.JComboBox;
 import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class BillingScreen extends JFrame {
 
@@ -16,6 +31,8 @@ public class BillingScreen extends JFrame {
 	
 	private JPanel contentPane;
 	JComboBox<String> comboBox = new JComboBox<>();
+	JLabel lblProductName = new JLabel("");
+	JLabel lblProductPhoto = new JLabel("");
 
 	public static void main(String[] args) {
 		BillingScreen frame = new BillingScreen();
@@ -32,10 +49,29 @@ public class BillingScreen extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		comboBox.setFont(new Font("Kokonor", Font.PLAIN, 16));
+		comboBox.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		comboBox.setBounds(150,25,200,30);
 		contentPane.add(comboBox);
 		fillCombo();
+		
+		JLabel lblChooseItem = new JLabel("Choose Item");
+		lblChooseItem.setFont(new Font("Kokonor", Font.PLAIN, 20));
+		lblChooseItem.setBounds(25, 32, 100, 20);
+		contentPane.add(lblChooseItem);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setBounds(362, 27, 117, 29);
+		btnSearch.addActionListener((e)->{
+			search();
+		});
+		contentPane.add(btnSearch);
+		
+		lblProductName.setFont(new Font("Kokonor", Font.PLAIN, 18));
+		lblProductName.setBounds(160, 67, 600, 30);
+		contentPane.add(lblProductName);
+		
+		lblProductPhoto.setBounds(150, 109, 1200, 600);
+		contentPane.add(lblProductPhoto);
 		
 		this.addWindowListener(new WindowAdapter() {
 			@Override
@@ -45,8 +81,38 @@ public class BillingScreen extends JFrame {
 		});
 	}
 	
+	private void search() {
+		String selectedProduct = (String) comboBox.getSelectedItem();
+		System.out.println("Selected product is " + selectedProduct);
+		try {
+			Product product = ProductDAO.getProductByName(selectedProduct);
+			lblProductName.setText(product.getName());
+			String images[] = product.getPath().split("\"");
+//			System.out.println(Arrays.toString(images));
+			System.out.println(images[1]);
+		    URL url = new URL(images[1]);
+		    Image image = ImageIO.read(url);
+		    image = image.getScaledInstance(800,600, java.awt.Image.SCALE_DEFAULT);
+		    lblProductPhoto.setIcon(new ImageIcon(image));
+//			lblProductPhoto.setIcon(new ImageIcon(BillingScreen.class.getResource("/com/company/billing/main/intro.png")));
+//			lblProductPhoto.setBounds(150, 109, 1200, 600);
+//			contentPane.add(lblProductPhoto);
+		}
+		catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void fillCombo() {
 		comboBox.addItem("Select Item");
+		try {
+			for(String productName : ProductDAO.getProductNames()) {
+				comboBox.addItem(productName); 
+			}
+		}
+		catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void askForClose() {
@@ -55,6 +121,4 @@ public class BillingScreen extends JFrame {
 			this.dispose();
 		}
 	}
-	
-	
 }
--- BillingSystem/src/com/company/billing/model/ProductDAO.java
+++ BillingSystem/src/com/company/billing/model/ProductDAO.java
@@ -2,6 +2,7 @@ package com.company.billing.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -46,7 +47,54 @@ public interface ProductDAO {
 		}
 	}
 	
-	public static ArrayList<Product> getProductNames() {
-		return null;
+	public static ArrayList<String> getProductNames() throws SQLException, ClassNotFoundException {
+		ArrayList<String> productNames = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = CommonDAO.getConnection();
+			statement = connection.prepareStatement("select name from product_mst");
+			resultSet = statement.executeQuery();
+			while(resultSet.next()) {
+				productNames.add( resultSet.getString("name") );
+			}
+		}
+		finally {
+			resultSet.close();
+			statement.close();
+			connection.close();
+		}
+		
+		return productNames;
+	}
+	
+	public static Product getProductByName(String name) throws SQLException, ClassNotFoundException {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		Product product = new Product();
+		try {
+			connection = CommonDAO.getConnection();
+			statement = connection.prepareStatement("select * from product_mst where name = ?");
+			statement.setString(1, name);
+			resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				product.setId(resultSet.getString("id"));
+				product.setName(resultSet.getString("name"));
+				product.setDescription(resultSet.getString("description"));
+				product.setPrice(resultSet.getDouble("price"));
+				product.setPath(resultSet.getString("imgpath"));
+				product.setBrand(resultSet.getString("brand"));
+				product.setCategory(resultSet.getString("category"));
+			}
+		}
+		finally {
+			resultSet.close();
+			statement.close();
+			connection.close();
+		}
+		
+		return product;	
 	}
 }
